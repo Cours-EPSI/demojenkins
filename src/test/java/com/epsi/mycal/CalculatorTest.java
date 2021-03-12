d82845db4e680ec08614e4719adb38358da7d49a
Thibault Doucet <thibault.doucet44@gmail.com>
2021-03-12 11:52:33 +0100

    :wrench: Updated tests

--- src/test/java/com/epsi/mycal/CalculatorTest.java
+++ src/test/java/com/epsi/mycal/CalculatorTest.java
@@ -1,33 +1,41 @@
 package com.epsi.mycal;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
-public class CalculatorTest extends TestCase {
+import static org.junit.Assert.*;
 
+public class CalculatorTest {
+
+    @Test
     public void testAdd() {
         int a = 2;
         int b = 3;
         assertEquals(5, Calculator.getInstance().add(a, b));
     }
 
+    @Test
     public void testSub() {
         int a = 5;
         int b = 3;
         assertEquals(2, Calculator.getInstance().sub(a, b));
     }
 
+    @Test
     public void testMult() {
         int a = 5;
         int b = 3;
         assertEquals(15, Calculator.getInstance().mult(a, b));
     }
 
+    @Test
     public void testDiv() {
         int a = 6;
         int b = 3;
         assertEquals(2, Calculator.getInstance().div(a, b));
     }
 
+    @Test
     public void testIsPair() {
         int a = 6;
         int b = 3;
@@ -35,6 +43,7 @@ public class CalculatorTest extends TestCase {
         assertFalse(Calculator.getInstance().isPair(b));
     }
 
+    @Test
     public void testIsPremier() {
         int a = 17;
         int b = 4;
